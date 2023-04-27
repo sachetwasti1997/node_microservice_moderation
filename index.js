30972536ca56d9caa8abafa2c4ffe7ef353da65b
sachetwasti1997 <wasti_sachet@optum.com>
2023-04-27 07:46:05 +0530

    Code Push

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', async (req, res) => {
+    const {type, data} = req.body;
+
+    if (type === 'COMMENT' && data.status === 'pending') {
+        const status = data.content.includes('sex') ? 'rejected' : 'approved';
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'COMMENT_MODERATED',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        }).catch(err => {
+            console.log(err.message);
+        });
+    }
+
+    res.status(201).send('CREATED');
+});
+
+app.listen(4003, () => {
+    console.log("Listening on port 4003");
+})
